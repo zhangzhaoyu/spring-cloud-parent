2d0f42a25c4557fbc1c1c529dbd27eb639c206fe
zhangzhaoyu <zhangzhaoyu0524@163.com>
2018-09-13 17:20:21 +0800

    add spring mvc

--- spring-integration/src/main/java/cn/cincout/distribute/spring/integration/hessian/client/HessianClientSample.java
+++ spring-integration/src/main/java/cn/cincout/distribute/spring/integration/hessian/client/HessianClientSample.java
@@ -15,7 +15,8 @@ public class HessianClientSample {
     private final static String HESSIAN_SERVICE_URL = "http://localhost:8080/hessian/hello";
     public static void main(String[] args) {
         HessianProxyFactory factory = new HessianProxyFactory();
-        factory.setReadTimeout(10000);
+        factory.setReadTimeout(5000);
+        factory.setConnectTimeout(5000);
 
         try {
             HelloService helloService = (HelloService) factory.create(HelloService.class, HESSIAN_SERVICE_URL);
--- spring-learn/spring-env/spring-boot-env/src/main/java/cn/cincout/distribute/spring/boot/env/controller/ApolloValueController.java
+++ spring-learn/spring-env/spring-boot-env/src/main/java/cn/cincout/distribute/spring/boot/env/controller/ApolloValueController.java
@@ -3,9 +3,14 @@ package cn.cincout.distribute.spring.boot.env.controller;
 import cn.cincout.distribute.spring.boot.env.service.ApolloConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 /**
@@ -29,5 +34,20 @@ public class ApolloValueController {
     @ResponseBody
     public Map<String, String> getApolloValue() {
         return apolloConfigService.getApolloConfigValue();
+    }
+
+    @PostMapping(value = "/test")
+    public String test(ModelMap modelMap, HttpServletRequest request, @RequestBody String val) {
+        System.out.println("val : " + val);
+        modelMap.put("val", val);
+        request.setAttribute("val", val);
+        return "forward:/forward";
+    }
+
+    @PostMapping(value = "/forward")
+    @ResponseBody
+    public String forward(HttpServletRequest request) {
+        System.out.println("forward val : " + request.getAttribute("val"));
+        return "good";
     }
 }
