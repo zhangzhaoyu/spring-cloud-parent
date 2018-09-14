f3ba0cceca569c20d0a3f256984b00c44e11decc
zhangzhaoyu <zhangzhaoyu0524@163.com>
2018-09-14 17:17:32 +0800

    add spring mvc

---
+++ spring-learn/spring-mvc-parent/spring-mvc/src/main/java/cn/cincout/distribute/spring/mvc/config/RootConfig.java
@@ -0,0 +1,13 @@
+package cn.cincout.distribute.spring.mvc.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by zhaoyu on 18-9-14.
+ *
+ * @author zhaoyu
+ * @sine 1.8
+ */
+@Configuration
+public class RootConfig {
+}
---
+++ spring-learn/spring-mvc-parent/spring-mvc/src/main/java/cn/cincout/distribute/spring/mvc/config/WebInitializer.java
@@ -0,0 +1,29 @@
+package cn.cincout.distribute.spring.mvc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * Created by zhaoyu on 18-9-14.
+ *
+ * @author zhaoyu
+ * @sine 1.8
+ */
+@EnableWebMvc
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] {RootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {AppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/*"};
+    }
+}
